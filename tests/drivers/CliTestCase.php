4bdd3dc66257adb8820967a1cff246cf4cfb65c9
charescape <charescape@outlook.com>
2022-04-05 20:14:09 +0800

    fix tests

--- tests/drivers/CliTestCase.php
+++ tests/drivers/CliTestCase.php
@@ -26,14 +26,14 @@ abstract class CliTestCase extends TestCase
     /**
      * @param array $cmd
      */
-    protected function runProcess($cmd)
+    protected function runProcess(array $cmd)
     {
         $cmd = $this->prepareCmd($cmd);
         $process = new Process($cmd);
         $process->mustRun();
 
         $error = $process->getErrorOutput();
-        $this->assertEmpty($error, "Can not execute '$cmd' command:\n$error");
+        $this->assertEmpty($error, "Can not execute '" . implode(' ', $cmd) . "' command:\n$error");
     }
 
     /**
